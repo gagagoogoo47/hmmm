4854ba6b546481012603138f5bfee08fc2e432f5
germ <tiachapong@gmail.com>
2026-01-04 15:23:05 +0530

    Add files via upload

---
+++ Strings in Py/index.py
@@ -0,0 +1,8 @@
+Yum = "i ate"
+Yummer = "potato nuggets"
+Yummy = "Eggs"
+Yummiest = "and rice"
+Oh_yeah = "for lunch today😃"
+
+sentence = f"{Yum} {Yummer}, {Yummy} {Yummiest} {Oh_yeah}"
+print(sentence)
