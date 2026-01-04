a3cd497f5d5c2a84759b6bea4be6a0520efc7bc8
germ <tiachapong@gmail.com>
2026-01-04 15:14:32 +0530

    Add files via upload

---
+++ Natural Numbers/index.py
@@ -0,0 +1,8 @@
+number = int(input("Enter number to be checked"))
+print ("number to be checked:", number)
+
+if type(number) == int and number > 0:
+    print("This is a natural number 😃.")
+else:
+    print("This is NOT a natural number 😡.")
+
