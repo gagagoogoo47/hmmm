84f517af530645efdc848242da9b611f0a5031bd
germ <tiachapong@gmail.com>
2026-02-03 19:31:27 +0530

    Add files via upload

--- Friendslop book/index.py
+++ Friendslop book/index.py
@@ -54,4 +54,52 @@ def menu():
 
     print("**********************************************************************")
 
-initial_slambook()
+print("\t\tSMARTPHONE DIRECTORY", flush=False)
+
+print("\tYou can now perform the following operation on this\nslambook\n")
+print("1. Add a new contact")
+print("2. Remove an existing contact")
+print("3. Delete all contacts")
+print("4. Search for a contact")
+print("5. Display all contacts")
+print("6. Exit phonebook")
+
+def add_contact(pb):
+    
+    for i in range(len(pb[0])):
+        if i == 0:
+            dip.append(str(input("Enter name: ")))
+        if i == 1:
+            dip.append(int(input("Enter number: ")))
+        if i == 2:
+            dip.append(str(input("Enter e-mail address: ")))
+        if i == 3:
+            dip.append(str(input("Enter date of birth(dd/mm/yy): ")))
+        if i == 4:
+            dip.append(str(input("Enter category (Family/Friends/work/Others): ")))
+    pb.append(dip)
+   
+    return pb
+def thanks():
+    
+    print("******************************************************")
+    print("Thank you for using our Slam Book.")
+    print("Please visit again!")
+    print("******************************************************")
+    sys.exit("Goodbye, have a nice day ahead!")
+
+print("........................................................")
+print("Hello dear Friends, welcome to our Slam Book")
+print("You may now proceed to explore this Slam Book and fill your details about your friends")
+
+ch = 1
+pb = initial_slambook()
+while ch in (1, 2, 3, 4, 5):
+    ch = menu()
+    if ch == 4:
+        pb = add_contact(pb)
+    else:
+        thanks()
+
+ # dis is the end 🙂.
+
